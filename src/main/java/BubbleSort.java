b1a15cc78b1e7b3066aa52396d5327857d732bec
Chou Zijing <X00217230216@njit.edu.cn>
2024-03-05 09:44:07 +0800

    bubble sort assignment

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Arrays;
+public class BubbleSort {
+    // array = {38,65,3,17,2}
+    public void bubbleSort(int [] array){
+        for(int i = 0; i< array.length-1;i++){
+            for(int j = 0;j<array.length-i-1;j++)
+                if(array[j]>array[j+1]) {//65<3;{2,3,17,38,65
+                    System.out.println("i value is" + i + "and j value is" + j);
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                }
+        }
+        System.out.println(Arrays.toString(array));
+    }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {0, 8, 3, 6, 7, 9};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.bubbleSort(array);
+        for (int i : array) {
+            System.out.print(i + ",");
+        }
+    }
 }
