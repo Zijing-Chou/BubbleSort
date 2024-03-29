ef76b78e322ebb90819718ca594e9315f8aa0268
Chou Zijing <161273373+Zijing-Chou@users.noreply.github.com>
2024-03-29 15:57:04 +0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0, 8, 3, 6, 7, 9};
+        int[] array = {9, 7 , 5 , 6, 3, 1};
         BubbleSort bubbleSort = new BubbleSort();
         bubbleSort.bubbleSort(array);
-        for (int i : array) {
+        for (int i=0;i<=6;i++) {
             System.out.print(i + ",");
         }
     }
